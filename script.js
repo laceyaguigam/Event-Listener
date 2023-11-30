ccb715e0a725f2ffb3bbe1a5937897a7856cfeed
lacey aguigam <laceyaguigam@yahoo.com>
2023-11-29 19:27:25 -0800

    added the notes for the second problem, however problem one and two do not work despite the code matching the instructor

--- script.js
+++ script.js
@@ -49,17 +49,24 @@ function setSize(newSize) {
 setSize(30); //should change the size 
 
 function handleArrows(event) {                  //e is used as an exception
-    if (event.key == "ArrowUp") {
+    if (size > 80) {
+        balloon.textContent = "💥";
+        document.body.removeEventListener("keydown", handleArrows);
+    } else  {
         setSize(size *1,1);
         event.preventDefault();
-    } else if (event.key == "ArrowDown") {
+    }
+     else if (event.key == "ArrowDown") {
         setSize(size * 0.9);
         event.preventDefault(); //prevents scroll
-    } else 
-    console.log("key pressed not used");
+    }
+     else {
+        console.log("key pressed not used");
+    }
+    
 }      
-
 document.body.addEventListener("keydown", handleArrows);
+
 
 
 
@@ -145,4 +152,44 @@ document.body.addEventListener("keydown", handleArrows);
 // });
 
 
+//class notes
+document.addEventListener("DOMContentLoad", function() {
 
+
+//store the elements as variables 
+
+const tab1 = document.getElementById("tab1");
+const tab2 = document.getElementById("tab2");
+const tab3 = document.getElementById("tab3");
+
+
+const tab1Link = document.getElementById("tab1Link");
+const tab2Link = document.getElementById("tab2Link");
+const tab3Link = document.getElementById("tab3Link");
+
+function hideAllContent() {
+    tab1.style.display = "none";
+    tab2.style.display = "none";
+    tab3.style.display = "none";
+}
+
+hideAllContent ();
+tab1.style.display = "block";
+
+
+tab1link.addEventListener("click", function(event) {
+    event.preventDefault();
+    hideAllContent();
+    tab1.style.display = "block";
+});
+tab2link.addEventListener("click", function(event) {
+    event.preventDefault();
+    hideAllContent();
+    tab2.style.display = "block";
+});
+tab3link.addEventListener("click", function(event) {
+    event.preventDefault();
+    hideAllContent();
+    tab3.style.display = "block";
+});
+});
